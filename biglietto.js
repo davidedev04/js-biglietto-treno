038c103b52e5791b85a64bdfec4a20b961ba022e
Davide Prencipe <davidedev04@gmail.com>
2023-11-17 16:23:39 +0100

    creazione sconti

---
+++ biglietto.js
@@ -0,0 +1,36 @@
+
+/* calcolo del prezzo del biglietto */
+
+// chiedi a user quanti chilometri vuole percorrere
+
+const userKilometers = prompt("Quanti chilometri vuoi percorrere?");
+console.log(userKilometers);
+
+// chiedi a user la sua età
+
+const userAge = prompt("Quanti anni hai?");
+console.log(userAge);
+
+// calcolo del prezzo
+
+const prezzoBiglietto = userKilometers * 0.21;
+console.log(prezzoBiglietto);
+
+// calcolo del prezzo scontato
+
+if (userAge < 18) {
+    prezzoScontato = prezzoBiglietto - ((prezzoBiglietto * 20) / 100);
+    var numb = prezzoScontato;
+    numb = numb.toFixed(2);
+
+} else if (userAge > 65) {
+    prezzoScontato = prezzoBiglietto - ((prezzoBiglietto * 40) / 100);
+    var numb = prezzoScontato;
+    numb = numb.toFixed(2);
+} else {
+    console.log(prezzoBiglietto);
+    var numb = prezzoScontato;
+    numb = numb.toFixed(2);
+}
+
+console.log(prezzoScontato);
